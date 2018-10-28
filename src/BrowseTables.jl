5e5e2376d451f842cc1997ebf4ecd08ff7f10798
n3n7i <25413904+n3n7i@users.noreply.github.com>
2018-10-28 13:38:25 +0800

    -- experimental browsetables -- 0.4.-1
    (mostly?) commented changes version

--- src/BrowseTables.jl
+++ src/BrowseTables.jl
@@ -1,3 +1,4 @@
+
 module xBrowseTables
 
 ##export write_html_table, open_html_table, TableOptions
@@ -23,41 +24,25 @@ const CssDir = DEFAULTCSSDIR
 
 const xDebug = true
 
-Base.@kwdef struct TableOptions
+struct RowId
+    id::Int
+end
+
+Base.show(io::IO, x::RowId) = write(io, "$(x.id)");
+
+
+const tdef = Dict([String => (class = "alignleft", ), Missing => (class = "likemissing", ), Nothing => (class = "likemissing", ), 
+					RowId => (class = "rowid", )]);
+
+Base.@kwdef struct xTableOptions
     css_path::AbstractString = DEFAULTCSSPATH
     css_inline::Bool = true
+	d::Dict = Dict(["td" => tdef]);
 end
 
 
-# high-level API
 
-## xDebug && print("")
-
-function write_html_table(filename::AbstractString, tables;
-                          title = "Table", caption = nothing,
-                          options::TableOptions = TableOptions())
-    ##
-xDebug && print("write html: ");
-
-    f = open(filename, "w")# do io
-    write(f, gen_html_table([tables], title=title, caption=caption, options=options));
-    close(f);
-   # end
-
-xDebug && println(" /write html");
-    nothing
-end
-
-function open_html_table(table; filename = tempname() * ".html", kwargs...)
-    write_html_table(filename, table; kwargs...)
-    DefaultApplication.open(filename)
-end
-
-
-## hookstring
-
-
-function gen_html_table(tables; title = "Table", caption = nothing, options = TableOptions())
+function gen_html_table(tables; title = "Table", caption = nothing, options = xTableOptions())
 
   ##filename::AbstractString, 
 
@@ -71,12 +56,15 @@ xDebug && print("gen html: ");
   io = IOBuffer();
 
   write(io, HTMLHEADSTART)
-  writetags(io -> writeescaped(io, title), io, "title")
+  
+  #writetags(io -> writeescaped(io, title), io, "title")
+  
+  writecell(io, title, kind="title");
 
   writestyle(io, options.css_path, options.css_inline)
 
   println(io, "</head>")    # close manually, opened in HTMLHEADSTART
-  writetags(io, "body"; bropen = true) do io
+  writetags(io, "body") #do io
 
     for table in tables
 
@@ -86,161 +74,147 @@ xDebug && print("gen html: ");
       ##@argcheck Tables.istable(table) "The table should support the interface of Tables.jl."
       println(io, "<div class='divstyle'>"); ## scroll properties attached 
 
-      writetags(io, "table"; bropen = true) do io
+      writetags(io, "table") #do io
                 writecaption(io, caption)
 
                 rows = table; ##rows = Tables.rows(table)
 
                 writeschema(io, schema_patch(table)); #Tables.schema(rows))
-                writetags(io, "tbody"; bropen = true) do io
+				
+                writetags(io, "tbody") #do io
                     for (id, row) in enumerate(rows)
                         writerow(io, options, merge((rowid_nXxN = RowId(id),), row)) ##obscured rowid
                         end
-                    end
-               end;
+                    #end
+               #end;
       println(io, "</div>"); ## endscroll
       end#for
                   
-    end;
+    #end;
   println(io, "</html>");
 
 xDebug && println(" /gen html ");
 
-
-  return take!(io);
+  
+  ##reesc = String(take!(io));
+  
+  ##print(io, reesc);
+  
+  return String(take!(io));
   end;
 
-
-  ##end # module ## bottom-up simpli
-  ## reverse?
-
-
-# table structure
-
-writecell(io, content::AbstractString; kind = "td", attributes = NamedTuple()) =
-    writetags(io -> writeescaped(io, content), io, kind; attributes = attributes)
-
-function writerow(io, options::TableOptions, nt::NamedTuple)
-
-xDebug && print("row: ");
-
-    writetags(io, "tr"; brclose = true) do io
-        for x in values(nt)
-
-xDebug && print("#r cell: ");
-
-            htmlcontent, attributes = cellwithattributes(options, x)
-            writecell(io, htmlcontent; attributes = attributes)
-        end
-    end
-
-xDebug && println("/row: ");
-
-
-end
-
+##function writerow(io, options::xTableOptions, nt::NamedTuple)  
+  
+  
 writecaption(io, ::Nothing) = nothing
 
-writecaption(io, str::AbstractString) = writetags(io -> writeescaped(io, str), io, "caption")
+#writecaption(io, str::AbstractString) = writetags(io -> writeescaped(io, str), io, "caption")
+
+writecaption(io, str::AbstractString) = writecell(io, str, kind="caption")
 
 schema_patch(z::Array{NamedTuple{Z, T}, N}) where{Z, T, N} = Z; ##Tables.Schema(Z, Z);
 
 schema_patch(z::Array{NamedTuple{Z}}) where{Z} = Z; ##Tables.Schema(Z, Z);
 
 
-writeschema(io, ::Nothing) = nothing
-
+  
+##function writecell(io, content::AbstractString; kind = "td", attributes = NamedTuple(), singletons = [])
+##function writetags(io::IO, tag::AbstractString; attributes = NamedTuple(), singletons = [])
+  
+  function writestyle(io::IO, path::AbstractString, inline::Bool)
+    if inline
+        ##writetags(io -> write(io, read(path, String)), io, "style")
+		writecell(io, read(path, String); kind="style");
+		
+    else
+	    writetags(io, "link", attributes = (href = "$path", rel="stylesheet", type="text/css"));
+        #println(io, raw"<link href=\"", path, raw"\" rel=\"stylesheet\" type=\"text/css\">")
+    end
+    nothing
+end
 
 function writeschema(io, sch)#::Tables.Schema)
 
 xDebug && print("schema: ");
 
-    writetags(io, "thead"; brclose = true) do io
-        writetags(io, "tr") do io
-            writecell(io, "#"; kind = "th") # row id
-            foreach(x -> writecell(io, escapestring(string(x)); kind = "th"), sch)
-        end
+    writetags(io, "thead"); ##; brclose = true) do io
+	writetags(io, "tr"); ## do io
+	writecell(io, "#"; kind = "th") # row id
+	
+    foreach(x -> writecell(io, "$x"; kind = "th"), sch) ##escapestring(string(x))
+	
+	writetags(io, "/tr></thead"); ## do io
+        #end
     end
 
-xDebug && println("/schema: ");
 
-end
+function cellwithattributes(setting::xTableOptions, kind, content)
+  pretty = IOBuffer();
+  show(pretty, content);
+  xcont = String(take!(pretty));
+  
+  xDebug && begin
+    println();
+	print(typeof(content), "  ");
+    print(haskey(setting.d, kind), " ", haskey(setting.d[kind], typeof(content)), ": ");
+	end;
+  
+  if(haskey(setting.d, kind) && haskey(setting.d[kind], typeof(content)))
+    return xcont, setting.d[kind][typeof(content)];
+	end;	
+  return xcont, NamedTuple()
+  end;
+      
 
-function writestyle(io::IO, path::AbstractString, inline::Bool)
-    if inline
-        writetags(io -> write(io, read(path, String)), io, "style")
-    else
-        println(io, raw"<link href=\"", path, raw"\" rel=\"stylesheet\" type=\"text/css\">")
-    end
-    nothing
-end
+# table structure
 
+function writerow(io, options::xTableOptions, nt)
 
-
-# cell formatting
+xDebug && print("row: ");
 
-"""
-\$(SIGNATURES)
-HTML representation of a cell, with attributes. Content should be escaped properly, as it is
-written to HTML directly.
-"""
-cellwithattributes(::TableOptions, x) = escapestring(string(x)), NamedTuple()
+    writetags(io, "tr") #do io
+        for x in values(nt)
 
-cellwithattributes(::TableOptions, x::Real) =
-    escapestring(string(x)), isfinite(x) ? NamedTuple() : (class = "nonfinite", )
+xDebug && print("#r cell: ");
 
-cellwithattributes(::TableOptions, str::AbstractString) =
-    escapestring(str), (class = "alignleft", )
+            htmlcontent, attributes = cellwithattributes(options, "td", x)
+            writecell(io, htmlcontent, attributes = attributes)
+            end
+        #end
 
-cellwithattributes(::TableOptions, x::Union{Missing,Nothing}) =
-    escapestring(repr(x)), (class = "likemissing", )
-
-struct RowId
-    id::Int
-end
-
-cellwithattributes(::TableOptions, rowid::RowId) =
-    escapestring(string(rowid.id)), (class = "rowid", )
+    end;
 
 
-
-# low-level HTML construction
+function writecell(io, content::AbstractString; kind = "td", attributes = NamedTuple(), singletons = [])
 
-function writeescaped(io, str::AbstractString)
-    for char in str
-        if char == '&'
-            write(io, "&amp;")
-        elseif char == '<'
-            write(io, "&lt;")
-        elseif char == '>'
-            write(io, "&gt;")
-        elseif char == '"'
-            write(io, "&quot;")
-        else
-            write(io, char)
-        end
-    end
-    nothing
-end
+    writetags( io, kind; attributes = attributes, singletons = [])
+	write(io, "$content");
+	writetags( io, "/$kind");
+	nothing
+	end;
+	
 
-escapestring(str::AbstractString) = (io = IOBuffer(); writeescaped(io, str); String(take!(io)))
+# low level api
 
-function writetags(f, io::IO, tag::AbstractString; bropen = false, brclose = bropen,
-                   attributes = NamedTuple())
+
+function writetags(io::IO, tag::AbstractString; attributes = NamedTuple(), singletons = [])
 
 xDebug && print("tag: ");
 
     write(io, "<", tag)
     for (k, v) in pairs(attributes)
-        write(io, " ", string(k), "=\"")
-        writeescaped(io, v)
-        write(io, "\"")
+        write(io, " $k = \"$v\" ")
+        #writeescaped(io, v)
+        #write(io, "\"")
+    end
+	for k in singletons
+        write(io, " $k ")
     end
     write(io, ">")
-    bropen && println(io)
-    f(io)
-    write(io, "</", tag, ">")
-    brclose && println(io)
+#    bropen && println(io)
+#    f(io)
+#    write(io, "</", tag, ">")
+#    brclose && println(io)
     nothing
 end
 
